46d18a5fa8e6b94f8134134d81b46e4caa6194d4
f0rtung <zadov.vasya@gmail.com>
2015-02-19 08:01:36 +0300

    v2

--- timetable.cpp
+++ timetable.cpp
@@ -22,6 +22,8 @@ void Timetable::on_pushButton_3_clicked()
 {
     QTextDocument* doc = ui->textEdit->document();
     QString str = doc->toPlainText();
+    str.remove(0,1);
+    str.remove(str.count()-1,1);
     ui->textEdit->setText(str);
     lst = str.split('\n');
 }
@@ -33,20 +35,20 @@ void Timetable::on_pushButton_2_clicked()
     ui->textEdit->setText(str);
     lst = str.split('\n');
     total = "Subject,Start Date,Start Time,End Date,End Time,All Day Event,Description,Location,Private";
-    for(int i = 0; i < lst.count()/7; ++i){
+    for(int i = 0; i < lst.count()/8; ++i){
         total +='\n';
-        QString data = lst.at(i*7);
+        QString data = lst.at(i*8 + 1);
         int index = data.indexOf('.');
         while(index != -1){
             data[index] = '/';
             index = data.indexOf('.');
         }
-        QString time = lst.at(i*7 + 2);
+        QString time = lst.at(i*8 + 3);
         QString timestart = time.split('-').at(0);
         QString timeend = time.split('-').at(1);
         timestart.insert(2,':');
         timeend.insert(2,':');
-        QString all = lst.at(i*7 + 4) + "," + data + "," + timestart + "," + data + "," + timeend + "," + "False" + "," + lst.at(i*7 + 3) + "," + lst.at(i*7 + 5) + ",True";
+        QString all = lst.at(i*8 + 5) + "," + data + "," + timestart + "," + data + "," + timeend + "," + "False" + "," + lst.at(i*8 + 4) + "," + lst.at(i*8 + 6) + ",True";
         total += all;
     }
     ui->textEdit->setText(total);
@@ -58,7 +60,7 @@ void Timetable::on_pushButton_clicked()
                     this,
                     "Save File",
                     ".",
-                    "Bin File (*.bin)"); // только в бинарный файл
+                    "cvs File (*.csv)");
      QFile file(filename);
      file.open(QIODevice::WriteOnly|QIODevice::Text);
      QTextStream out(&file);
